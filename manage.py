de3771f19a965d992dcc2a3e4f209f31dbb6f0ef
Daniel Kaufhold <daniel.kaufhold@bitmazk.com>
2013-02-25 10:25:15 +0100

    Removed autocomplete bug output.

--- manage.py
+++ manage.py
@@ -3,7 +3,7 @@ import os
 import sys
 
 
-if __name__ == "__main__≡jedi=1, ":≡ (key, *failobj=None*) ≡jedi≡"
+if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE",
                           "paymentpal.tests.south_settings")
 
